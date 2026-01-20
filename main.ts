1d2821798481aa39268272c4d4382ed8fd1739ea
Jmeow <j@jmeow.net>
2026-01-19 21:48:12 -0600

    Implemented a few utility functions
    Joining and leaving channels, and checking how many members are in a channel

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+const edgeNodeId = "E09V59WQY1E"
 const tokenFile = await Deno.readTextFile(Deno.env.get("TOKEN_FILE_PATH")!)
 const tokens: { xoxc: string; xoxd: string }[] = []
 tokenFile
@@ -7,8 +8,8 @@ tokenFile
         const values = line.split(" ")
         tokens[parseInt(values[0])] = { xoxc: values[2], xoxd: values[3] }
     })
-console.log(tokens)
-for (let i = 1; i <= 99; i++) {
+const doTokenCheck = false
+for (let i = 1; i <= 99 && doTokenCheck; i++) {
     try {
         const userBootResponse = await (
             await fetch(
@@ -35,3 +36,80 @@ for (let i = 1; i <= 99; i++) {
         throw new Error("invalid tokens for id " + i)
     }
 }
+async function leaveChannel(id: string) {
+    for (let i = 1; i <= 99; i++) {
+        console.log(
+            await (
+                await fetch(
+                    "https://hackclub.enterprise.slack.com/api/conversations.leave",
+                    {
+                        headers: {
+                            "Content-Type":
+                                "multipart/form-data; boundary=----boundary",
+                            Cookie: "d=" + tokens[i].xoxd,
+                        },
+                        body:
+                            '------boundary\r\nContent-Disposition: form-data; name="token"\r\n\r\n' +
+                            tokens[i].xoxc +
+                            '\r\n------boundary\r\nContent-Disposition: form-data; name="channel"\r\n\r\n' +
+                            id +
+                            "\r\n------boundary--\r\n",
+                        method: "POST",
+                    },
+                )
+            ).json(),
+        )
+        console.log(i + " left channel " + id)
+    }
+}
+async function joinChannel(id: string, botId: number) {
+    console.log(
+        await (
+            await fetch(
+                "https://hackclub.enterprise.slack.com/api/conversations.join",
+                {
+                    headers: {
+                        "Content-Type":
+                            "multipart/form-data; boundary=----boundary",
+                        Cookie: "d=" + tokens[botId].xoxd,
+                    },
+                    body:
+                        '------boundary\r\nContent-Disposition: form-data; name="token"\r\n\r\n' +
+                        tokens[botId].xoxc +
+                        '\r\n------boundary\r\nContent-Disposition: form-data; name="channel"\r\n\r\n' +
+                        id +
+                        "\r\n------boundary--\r\n",
+                    method: "POST",
+                },
+            )
+        ).json(),
+    )
+    console.log(botId + " joined channel " + id)
+}
+async function getChannelMemberCount(id: string) {
+    const response = await (
+        await fetch(
+            "https://edgeapi.slack.com/cache/" + edgeNodeId + "/users/counts",
+            {
+                headers: {
+                    "Content-Type": "text/plain;charset=UTF-8",
+                    Cookie: "d=" + tokens[1].xoxd,
+                },
+                body: JSON.stringify({
+                    token: tokens[1].xoxc,
+                    channel: id,
+                    as_admin: false,
+                    enterprise_token: tokens[1].xoxc,
+                }),
+                method: "POST",
+            },
+        )
+    ).json()
+    return response.counts.people
+}
+async function requiredMemberCountChange(id: string) {
+    return 67 - (await getChannelMemberCount(id))
+}
+console.log(await requiredMemberCountChange("C0A9KBWRNP7"))
+// await leaveChannel("C0A9KBWRNP7")
+// await joinChannel("C0A9KBWRNP7", 5)
